4e691faa1d60109ce993eaf4aca1e96c52ba9925
sarbajit Bhattacharjee <webdev@ranktechsolutions.com>
2022-09-07 11:47:22 +0530

    initial code

---
+++ src/test/java/com/aws/pipeline/PipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
