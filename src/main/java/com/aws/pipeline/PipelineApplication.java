4535eba7a0fbd3f07ce7f7bab1303f69aec01ef6
sarbajit Bhattacharjee <webdev@ranktechsolutions.com>
2022-09-07 14:12:22 +0530

    Update PipelineApplication.java

--- src/main/java/com/aws/pipeline/PipelineApplication.java
+++ src/main/java/com/aws/pipeline/PipelineApplication.java
@@ -2,10 +2,17 @@ package com.aws.pipeline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PipelineApplication {
 
+	@GetMapping("/")
+	public String hello() {
+		return "Hello";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(PipelineApplication.class, args);
 	}
